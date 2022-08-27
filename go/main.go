00fbfe1a17712b91fda13836f4142a32137bd12e
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2022-08-27 17:09:23 +0900

    :bug:fix1

--- go/main.go
+++ go/main.go
@@ -1957,7 +1957,7 @@ func (h *Handler) addExpToCard(c echo.Context) error {
 	masterDBRWM.RLock()
 	defer masterDBRWM.RUnlock()
 	var im ItemMaster
-	if err = h.MasterDB.Get(&im, "SELECT * FROM item_masters WHERE id=?", cardID); err != nil {
+	if err = h.MasterDB.Get(&im, "SELECT * FROM item_masters WHERE id=?", uc.CardID); err != nil {
 		if err == sql.ErrNoRows {
 			return errorResponse(c, http.StatusNotFound, err)
 		}

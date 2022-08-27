294b2dcb5ba09ef28ed588fe9c01f037788a2d9b
sapphi-red <green@sapphi.red>
2022-08-27 15:07:48 +0900

    test

--- sql/init.sh
+++ sql/init.sh
@@ -8,6 +8,9 @@ ISUCON_DB_USER=${ISUCON_DB_USER:-isucon}
 ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-isucon}
 ISUCON_DB_NAME=${ISUCON_DB_NAME:-isucon}
 
+echo $ISUCON_DB_HOST
+exit 1
+
 mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
 		--host "$ISUCON_DB_HOST" \
@@ -36,4 +39,4 @@ echo "LOAD DATA INFILE '${SECURE_DIR}5_user_presents_not_receive_data.tsv' REPLA
         -p"$ISUCON_DB_PASSWORD" \
         --host "$ISUCON_DB_HOST" \
         --port "$ISUCON_DB_PORT" \
-        "$ISUCON_DB_NAME" 
+        "$ISUCON_DB_NAME"
